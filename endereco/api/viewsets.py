1807d80505ecff919e6124050354faa1a45ff9ca
Leonardo Moretti <leonardonabarro2003@gmail.com>
2023-01-21 17:55:13 -0300

    'primeiro commit'

---
+++ endereco/api/viewsets.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+
+from endereco.models import Endereco
+
+from .serializers import EnderecoSerializer
+
+
+class EnderecoViewSet(ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
---
+++ pontos_turisticos/urls.py
@@ -0,0 +1,36 @@
+"""pontos_turisticos URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+
+from atracoes.api.viewsets import AtracaoViewSet
+from avaliacoes.api.viewsets import AvaliacaoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
+from core.api.viewsets import PontoTuristicoViewSet
+from endereco.api.viewsets import EnderecoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracao', AtracaoViewSet)
+router.register(r'endereco', EnderecoViewSet)
+router.register(r'comentario', ComentarioViewSet)
+router.register(r'avaliacao', AvaliacaoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
