f5e1ed852fbb85eed0feeaf2d5945bf270684958
Leonardo Moretti <leonardonabarro2003@gmail.com>
2023-02-12 17:36:29 -0300

    modificando o serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 
+from atracoes.api.serializers import AtracaoSerializer
 from core.models import PontoTuristico
+from endereco.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
         fields = ("id", "nome", "descricao", "aprovado",
-                  "foto")
+                  "foto", "atracoes", "comentario", "avaliacoes", "endereco")
